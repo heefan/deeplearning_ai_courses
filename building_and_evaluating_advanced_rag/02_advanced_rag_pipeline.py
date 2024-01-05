083d71a81c0e2a897ef73925e36c282f63325d97
li_tian <heefan@msn.com>
2024-01-05 12:29:00 +0800

    close #2, BEAR - lesson1 - basic RAG pipeline [#2]

--- building_and_evaluating_advanced_rag/02_advanced_rag_pipeline.py
+++ building_and_evaluating_advanced_rag/02_advanced_rag_pipeline.py
@@ -12,3 +12,27 @@ log_info(type(documents))
 log_info(len(documents))
 log_info(type(documents[0]))
 log_info(documents[0])
+
+
+##### Basic RAG Pipeline #####
+from llama_index import Document
+
+document = Document(text="\n\n".join([doc.text for doc in documents]))
+
+from llama_index import VectorStoreIndex
+from llama_index import ServiceContext
+from llama_index.llms import OpenAI
+
+llm = OpenAI(model="gpt-3.5-turbo", temperature=0.1)
+service_context = ServiceContext.from_defaults(
+    llm=llm,
+    embed_model="local:BAAI/bge-small-en-v1.5"
+)
+index = VectorStoreIndex.from_documents([document], service_context=service_context)
+
+query_engine = index.as_query_engine()
+response = query_engine.query(
+    "what are steps to take when finding projects to build your experience?"
+)
+
+log_info(str(response))
