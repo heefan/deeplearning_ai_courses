2a21c51b489831d95aa72581f006b88464270eea
li_tian <heefan@msn.com>
2024-01-09 22:26:19 +0800

    wip for l2

--- building_and_evaluating_advanced_rag/l2_rag_raid_of_metrics/rag_triad.py
+++ building_and_evaluating_advanced_rag/l2_rag_raid_of_metrics/rag_triad.py
@@ -1,11 +1,20 @@
+import nest_asyncio
+
+import helper.utils as utils
 from helper.utils import build_sentence_window_index, get_sentence_window_query_engine, get_prebuilt_trulens_recorder
 from helper.logger import  log_info
 import openai
-from openai import OpenAI
+from llama_index.llms import OpenAI
 from llama_index import SimpleDirectoryReader, Document, VectorStoreIndex, ServiceContext
+import nest_asyncio
+from trulens_eval import OpenAI as fOpenAI
+from trulens_eval import Feedback, TruLlama
+import numpy as np
 
+
+nest_asyncio.apply()
+provider = fOpenAI()
 pdf_book = "testing_assets/eBook-How-to-Build-a-Career-in-AI.pdf"
-
 openai.api_key = utils.get_openai_api_key()
 llm = OpenAI(model="gpt-3.5-turbo", temperature=0.1)
 
@@ -26,3 +35,18 @@ sentence_query_engine = get_sentence_window_query_engine(sentence_index)
 
 output = sentence_query_engine.query("How do you create AI portfolio?")
 log_info(output.response)
+
+
+
+##########################################################
+#  Feedback Function
+##########################################################
+# Answer Relevance
+f_qa_relevance = Feedback(
+    provider.relevance_with_cot_reasons,
+    name="Answer Relevance"
+).on_input_output()
+
+
+# Context Relvance
+
