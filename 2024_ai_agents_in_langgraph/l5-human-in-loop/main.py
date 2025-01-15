3b009a4710dcd2f44dd1f7a3cc7406efa67abccd
li_tian <heefan@msn.com>
2025-01-15 14:05:27 +0800

    #2024, #dl-langgraph, l5 human in loop

---
+++ 2024_ai_agents_in_langgraph/l5-human-in-loop/main.py
@@ -0,0 +1,37 @@
+## Human in Loop
+
+from dotenv import load_dotenv
+from langgraph.graph import StateGraph, END
+from typing import TypedDict, Annotated
+import operator
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, ToolMessage
+from langchain_openai import ChatOpenAI
+from langchain_community.tools.tavily_search import TavilySearchResults
+from langgraph.checkpoint.sqlite import SqliteSaver
+from uuid import uuid4
+from langchain_core.messages import AnyMessage, SystemMessage, HumanMessage, AIMessage
+
+_ = load_dotenv()
+
+def reduce_messages(left: list[AnyMessage], right: list[AnyMessage]) -> list[AnyMessage]:
+    # assign ids to messages that don't have them
+    for message in right:
+        if not message.id:
+            message.id = str(uuid4())
+    # merge the new messages with the existing messages
+    merged = left.copy()
+    for message in right:
+        for i, existing in enumerate(merged):
+            # replace any existing messages with the same id
+            if existing.id == message.id:
+                merged[i] = message
+                break
+        else:
+            # append any new messages to the end
+            merged.append(message)
+    return merged
+
+class AgentState(TypedDict):
+    messages: Annotated[list[AnyMessage], reduce_messages]
+
+tool = TavilySearchResults(max_results=2)
